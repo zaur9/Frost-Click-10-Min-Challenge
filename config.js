2bb03dfbaa9778e9361997f5bc0f356238cd1ab6
zaur9 <116566845+zaur9@users.noreply.github.com>
2025-12-07 17:03:55 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 export const CONFIG = {
   // ⚠️ ЗАМЕНИ НА СВОЙ АДРЕС КОНТРАКТА!
-  CONTRACT_ADDRESS: "0xE3d9AD4B407F7FdF6B6f2528188185686EAeac89",
+  CONTRACT_ADDRESS: "0xdACb6484017B54322bd7918C20310bA3fCADA9F0",
   
   GAME_DURATION: 10 * 60 * 1000, // 10 минут
   SOMNIA_CHAIN_ID: '0x13a7',     // 5031 в hex
