86ff417a8bd1ad753e85175dab2d6ec14004708e
zaur9 <116566845+zaur9@users.noreply.github.com>
2026-04-18 14:19:04 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import '@rainbow-me/rainbowkit/styles.css';
 import App from './App';
-import { AppProviders } from './providers/AppProviders';
 import './style.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <AppProviders>
-      <App />
-    </AppProviders>
+    <App />
   </StrictMode>
 );
